20b64436b3790aa791bf150b57598928610c25ea
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-17 08:58:48 -0300

    Modelar classe Candidato

--- src/br/com/zup/Exercicio2Revisao/Candidato.java
+++ src/br/com/zup/Exercicio2Revisao/Candidato.java
@@ -1,4 +1,46 @@
 package br.com.zup.Exercicio2Revisao;
 
 public class Candidato {
+    private String nome;
+    private double numeroMatricula;
+    private double nota;
+
+    public Candidato(String nome, double numeroMatricula, double nota) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(double numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Nome: " + nome);
+        retorno.append("Número de matrícula: " + numeroMatricula);
+        retorno.append("Nota: " + nota);
+        return retorno.toString();
+    }
 }
