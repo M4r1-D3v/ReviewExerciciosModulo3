993f33367c12681859ede4a60c96b91a5d73b314
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-17 09:02:55 -0300

    Modelar classe ServicoCandidato

--- src/br/com/zup/Exercicio2Revisao/ServicoCandidato.java
+++ src/br/com/zup/Exercicio2Revisao/ServicoCandidato.java
@@ -1,4 +1,27 @@
 package br.com.zup.Exercicio2Revisao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoCandidato {
+    private List<Candidato>candidatos = new ArrayList<>();
+
+    public ServicoCandidato(List<Candidato> candidatos) {
+        this.candidatos = candidatos;
+    }
+
+    public List<Candidato> getCandidatos() {
+        return candidatos;
+    }
+
+    public void setCandidatos(List<Candidato> candidatos) {
+        this.candidatos = candidatos;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Candidatos: " + candidatos);
+        return retorno.toString();
+    }
 }
