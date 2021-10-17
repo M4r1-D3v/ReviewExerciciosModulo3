28c138cc5e70b99974b15d24bfde84661bef18d1
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-17 10:55:34 -0300

    Criar entrada de dados do método adicionarCandidato

--- src/br/com/zup/Exercicio2Revisao/Sistema.java
+++ src/br/com/zup/Exercicio2Revisao/Sistema.java
@@ -6,10 +6,18 @@ public class Sistema {
 
     //método para a entrada de dados
 
-    private static Scanner entradaDados(String mensagem){
+    private static Scanner entradaDados(String mensagem) {
         System.out.println(mensagem);
         return new Scanner(System.in);
-    }
 
+    }
+    //método para receber a entrada de dados no cadastro de candidatos:
+
+    public static Candidato adicionarCandidato() {
+        String nome = entradaDados("Por favor digite o nome do candidato: ").nextLine();
+        String numeroMatricula = entradaDados("Por favor, digite o número de matrícula do candidato: ").nextLine();
+        double nota = entradaDados("Por favor, digite a nota do candidato: ").nextDouble();
+        return ServicoCandidato.adicionarCandidato(nome, numeroMatricula, nota);
+    }
 
 }
