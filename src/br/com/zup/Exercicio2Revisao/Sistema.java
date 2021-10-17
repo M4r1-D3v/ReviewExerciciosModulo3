5cf21949853d4749b660ce48da9467e6b6973303
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-17 11:10:06 -0300

    Criar método para exbir as opções do menu

--- src/br/com/zup/Exercicio2Revisao/Sistema.java
+++ src/br/com/zup/Exercicio2Revisao/Sistema.java
@@ -21,9 +21,22 @@ public class Sistema {
     }
 
     //método para receber a entrada de dados do método excluirCandidato
-    public static void excluirCandidato(){
+    public static void excluirCandidato() {
         String numeroMatricula = entradaDados("Por favor, digite o número de matrícula do candidato que deseja excluir: ").nextLine();
         ServicoCandidato.excluirCandidato(numeroMatricula);
+    }
+
+    //método para exibir as opções do menu
+    public static void menu() {
+        System.out.println("\n ");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Olá, seja bem vinde ao sistema de gerenciamento de candidatos < Pagou, pa$$ou >");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("\n ");
+        System.out.println("Para adicionar um candidato, por favor digite [1]");
+        System.out.println("Para listar os candidatos cadastrados, por favor digite [2]");
+        System.out.println("Para excluir um candidato, por favor digite [3]");
+        System.out.println("Para sair do sistema, por favor digite [4]");
     }
 
 }
